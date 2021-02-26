f7a9262da0a0c86a9a9c2cb9c707aa35b3a2275c
Dominik Urbaez Gomez <dominiug@yahoo.com>
2021-02-26 14:34:53 +0100

    Initial Commit

---
+++ GifsList_DemoApp/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  GifsList_DemoApp
+//
+//  Created by Dominik Urbaez Gomez on 26/02/2021.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
